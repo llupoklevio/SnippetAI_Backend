cd66a4ebce5c44a4793b97ae9353585cd8cb44e4
klevio.llupo <klevio.llupo@telematicainformatica.it>
2026-02-17 14:37:24 +0100

    feat: initialize db connection on server startup

--- src/type/data-source/connectionType.ts
+++ src/type/data-source/connectionType.ts
@@ -4,6 +4,7 @@ const connectionType = z.object({
     dbName: z.string(),
     serverName: z.enum(["postgres", "mysql"]),
     synchronize: z.boolean(),
+    database: z.string()
 })
 
 export type IConnectionType = z.infer<typeof connectionType>;
