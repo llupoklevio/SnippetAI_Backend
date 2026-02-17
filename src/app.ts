cc84b017d689da1712ee0290a8a2cd78d8fa0e04
klevio.llupo <klevio.llupo@telematicainformatica.it>
2026-02-17 11:52:28 +0100

    chore: base setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+const app = express()
+
+/** Per leggere il req.body **/
+app.use(express.json())
+
+/** Swagger attraverso la libreria Zod **/
+
+/** Routes **/
+
+/** Middleware **/
+
+export default app
