ffb77472ecfe97a9de6bd92aa636841a84639924
klevio.llupo <klevio.llupo@telematicainformatica.it>
2026-02-23 21:09:40 +0100

    ref: initial implementation of login

---
+++ src/auth/type/validatorTypeLogin.ts
@@ -0,0 +1,28 @@
+import { extendZodWithOpenApi, ZodOpenAPIMetadata } from "@asteasolutions/zod-to-openapi";
+import { z } from "zod";
+
+extendZodWithOpenApi(z);
+
+function op<T extends z.ZodTypeAny>(schema: T, meta: Partial<ZodOpenAPIMetadata>): T {
+    return (schema as any).openapi(meta);
+}
+
+export const loginValidator = z.object({
+    email: op(
+        z.string()
+            .trim()
+            .min(6, "Email is required")
+            .refine((val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val), "Invalid email address"),
+        { example: "kleviollupo@gmail.com" }
+    ),
+    password: op(
+        z.string()
+            .trim()
+            .min(6, "Password is required with minimum 6 characters")
+            .regex(/[A-Z]/, "One word must be uppercased")
+            .regex(/[0-9]/, "One ore more numbers must be on password"),
+        { example: "Klupo03" }
+    ),
+});
+
+export type typeLoginValidator = z.infer<typeof loginValidator>;
