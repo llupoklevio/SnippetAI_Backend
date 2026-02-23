d8157b459d543bea9705154cc81cb944afe124a3
klevio.llupo <klevio.llupo@telematicainformatica.it>
2026-02-23 21:09:40 +0100

    ref: initial implementation of login

---
+++ src/auth/controller/authLogin.ts
@@ -0,0 +1,24 @@
+import {Request, Response} from "express"
+
+export const login = async(req: Request, res: Response) => {
+    /** validator verificated
+     *
+     * password ha una lunghezza minima di 6 , alemno una lettera maiuscola e almeno un numero
+     * email una lunghezza minima di 6, formato email
+     *
+     * **/
+
+    const dataToLogin = (req as any).validDataBody;
+    req.log.info(`${dataToLogin.email} sta tentando di loggarsi`)
+
+    /**
+     * Service:
+     *      controllo che email è nel db
+     *      che la password coincida
+     *      generare token
+     * */
+
+    res.json({
+        message: "ok"
+    })
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,9 +3,12 @@ import {register} from "../auth/controller/authController.js";
 import {validationSchemaBody} from "../middleware/validation/validationSchemaBody.js";
 import {registerValidator} from "../auth/type/validatorTypeRegister.js";
 import {authLimiter} from "../RateLimiting/rate.js";
+import {login} from "../auth/controller/authLogin.js";
+import {loginValidator} from "../auth/type/validatorTypeLogin.js";
 
 const router = Router()
 
 router.post("/register",authLimiter,validationSchemaBody(registerValidator),register)
+router.post('/login',validationSchemaBody(loginValidator),login)
 
 export default router
