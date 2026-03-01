2c024fb9564a9a2ff822dd83f2a0aba211b5ebd2
klevio.llupo <klevio.llupo@telematicainformatica.it>
2026-03-01 16:23:25 +0100

    ref: implements migration for prod

--- src/type/data-source/connectionType.ts
+++ src/type/data-source/connectionType.ts
@@ -4,7 +4,13 @@ const connectionType = z.object({
     dbName: z.string(),
     serverName: z.enum(["postgres", "mysql"]),
     synchronize: z.boolean(),
-    database: z.string()
+    database: z.string(),
+    migrationsSetup: z.object({
+        migrations: z.array(z.string()),
+        migrationsRun: z.boolean().optional(),
+        migrationsTableName: z.string().optional(),
+        migrationsTransactionMode: z.string().optional(),
+    }).optional()
 })
 
 export type IConnectionType = z.infer<typeof connectionType>;
